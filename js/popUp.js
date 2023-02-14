86d5bdc5d1932d43b09e7b8574239285c65ea302
= <55615890+Kevin-Romero@users.noreply.github.com>
2023-02-13 19:40:17 -0800

    Added fade and pop up effects to page section

---
+++ js/popUp.js
@@ -0,0 +1,18 @@
+// * Pop Up
+
+window.addEventListener('scroll', () => {
+
+    const element = document.querySelectorAll(`.popUp`);
+
+    let i = 0;
+
+    while(i < element.length){
+
+        if(window.pageYOffset > (document.querySelector("#skills").offsetTop/1.6)){
+                element[i].classList.add(`showPopUp`);
+        }else{
+            element[i].classList.remove(`showPopUp`);
+        }
+        i++;
+    }
+});
